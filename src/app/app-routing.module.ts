ca4e74b5b2210ec0eac9df2c0588641c3b0aa8ed
Eduardo Cordero <educf93@gmail.com>
2019-01-29 11:58:24 +0100

    register and login with styles, no functional

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginViewComponent } from './login-view/login-view.component';
+import { RegisterViewComponent } from './register-view/register-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login',
+    component: LoginViewComponent
+  },
+  {
+    path:'register',
+    component: RegisterViewComponent
+  },
+  {
+    path:'',
+    component: LoginViewComponent
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
