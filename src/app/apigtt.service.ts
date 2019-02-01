e3340c3292a3fb83d7ed78cf16c3cee0a0e27d8f
Eduardo Cordero <educf93@gmail.com>
2019-02-01 17:13:43 +0100

    services added and register done

---
+++ src/app/apigtt.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpClient,HttpHeaders} from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApigttService {
+  loginEndpoint:string = 'api/auth';
+  registerEndpoint:string = 'api/users';
+  userType:string;
+  constructor(private http:HttpClient) { }
+
+  loginUser(username,password):any{
+    const body = {username,password}
+    return this.http.post(this.loginEndpoint,body).toPromise();
+  }
+  registerUser(username:string,email:string,password:string,role:string):any{
+
+    const body = {username,password,email,role}
+   
+    console.log(JSON.stringify(body));
+    return this.http.post(this.registerEndpoint,body).toPromise();
+  }
+
+}
--- src/app/login-view/login-view.component.ts
+++ src/app/login-view/login-view.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApigttService } from '../apigtt.service';
 
 @Component({
   selector: 'app-login-view',
   templateUrl: './login-view.component.html',
   styleUrls: ['./login-view.component.css']
 })
-export class LoginViewComponent implements OnInit {
+export class LoginViewComponent {
+  user:string;
+  password:string;
+  error:any;
+  constructor(private router: Router, private api:ApigttService) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  login(){
+    const {user, password} = this;
+    this.api.loginUser(user.trim(),password.trim()).then(() =>{
+      this.error = undefined;
+      this.router.navigate(['/panel-view'])
+    }).catch(error =>{
+      this.error = error
+    });
   }
 
 }
