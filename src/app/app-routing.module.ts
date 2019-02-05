ea030363e073a2be431d2dbb010b63a8370224be
Eduardo Cordero <educf93@gmail.com>
2019-02-05 12:32:12 +0100

    login with jwt and styles for login error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginViewComponent } from './login-view/login-view.component';
 import { RegisterViewComponent } from './register-view/register-view.component';
+import { PanelViewComponent } from './panel-view/panel-view.component';
 
 const routes: Routes = [
   {
@@ -15,7 +16,12 @@ const routes: Routes = [
   {
     path:'',
     component: LoginViewComponent
-  }];
+  },
+  {
+    path:'panel-view',
+    component: PanelViewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/login-view/login-view.component.ts
+++ src/app/login-view/login-view.component.ts
@@ -11,15 +11,29 @@ export class LoginViewComponent {
   user:string;
   password:string;
   error:any;
+  showError:boolean = false;
+  jwt:string;
   constructor(private router: Router, private api:ApigttService) { }
 
   login(){
     const {user, password} = this;
-    this.api.loginUser(user.trim(),password.trim()).then(() =>{
-      this.error = undefined;
-      this.router.navigate(['/panel-view'])
-    }).catch(error =>{
-      this.error = error
+    this.error = "Nombre de usuario o contraseña erronea"
+    this.api.loginUser(user.trim(),password.trim()).then((result) =>{
+      // if(result.status() === 401){
+      //   this.error;
+      //   console.log(this.error);
+      // }
+      this.error=undefined;
+      console.log(result);
+      
+      this.jwt = JSON.parse(result).token;
+      
+      console.log(this.jwt);
+      this.router.navigate(['/panel-view']);
+    }).catch((error) =>{
+      this.error;
+      this.showError=true;
+      console.log(error);
     });
   }
 
