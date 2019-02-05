e3c608df0807d2177106266d185ebd0c47e8e6e3
Eduardo Cordero <educf93@gmail.com>
2019-02-05 15:40:46 +0100

    navbar panel component added and panel table for certificates with css

--- src/app/data-model.ts
+++ src/app/data-model.ts
@@ -1,2 +1,24 @@
 export interface DataModel {
+    user:Array<Users>
+    certificate:Array<Certificates>
+}
+
+export interface Users{
+    iduser:number,
+    username:string,
+    jwt:string
+}
+
+export interface Certificates{
+    alias:string,
+    entity:string,
+    serialNum:number,
+    subject:string,
+    expireDate: Date;
+    password:string,
+    idOrg: number,
+    clientName:string,
+    emailRenov:string,
+    repo:string,
+    observations:string
 }
