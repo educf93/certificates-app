33b492a39da166e136f2ba763cbdb88e24358379
Eduardo Cordero <educf93@gmail.com>
2019-02-13 10:22:37 +0100

    Changes in data-model, view and ts done. Todo: connect app to jira

--- src/app/jira-config-view/jira-config-view.component.ts
+++ src/app/jira-config-view/jira-config-view.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Jira } from '../data-model';
+import { ApigttService } from '../apigtt.service';
 
 @Component({
   selector: 'app-jira-config-view',
   templateUrl: './jira-config-view.component.html',
   styleUrls: ['./jira-config-view.component.css']
 })
-export class JiraConfigViewComponent implements OnInit {
+export class JiraConfigViewComponent {
+  user:string;  
+  password:string;
+  proyect:string;
+  architecture:string;
+  team:string;
+  url:string;  
+  exploitation:string;
+  component:string;
+  descripition:string;
+  jiraData:Jira
+  constructor(private api:ApigttService) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  createJiraTask(){
+    const jiraData = {user:this.user,
+      password:this.password,
+      proyect:this.proyect,
+      architecture:this.architecture,
+      team:this.team,
+      url:this.url,
+      exploitation:this.exploitation,
+      component:this.component,
+      descripition:this.descripition,
+      jiraData:this.jiraData
+    }
+    this.api.validateUser(jiraData)
+    .then(()=>this.api.addJiraTask(jiraData))
+    .catch();
   }
 
 }
